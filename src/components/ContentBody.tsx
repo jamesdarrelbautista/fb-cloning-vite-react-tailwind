75878af454eaecf0b0e464e316de35a946591d4a
James Darrel Bautista <jamesdarrel.bautista@g.msuiit.edu.ph>
2025-02-04 06:55:12 +0800

    finished initial content body

--- src/components/ContentBody.tsx
+++ src/components/ContentBody.tsx
@@ -1,13 +1,15 @@
+import LowerContent from "./LowerContent";
+
 const ContentBody = () => {
   return (
     <>
-      <div className="flex flex-col items-center bg-neutral-700 h-screen">
-        <div className="bg-indigo-800 relative w-[75%] h-[430px] rounded-b-md">
+      <div className="flex flex-col items-center bg-neutral-700">
+        <div className="bg-indigo-800 relative w-[75%] h-[450px] rounded-b-md">
           <div className="overflow-visible w-[160px] h-[160px] bg-slate-400 rounded-full absolute top-[90%] left-10 border-4 border-neutral-700"></div>
-          <div className="absolute top-[100%] left-[19%]">
-            <div className="flex flex-col gap-2 text-4xl font-bold text-slate-200 ">
+          <div className="absolute top-[101%] left-[19%]">
+            <div className="flex flex-col gap-2 text-3xl font-bold text-slate-200 ">
               James Darrel Bautista
-              <div className="text-sm font-md">736 friends</div>
+              <div className="text-sm font-medium">736 friends</div>
               <div className="flex relative">
                 <div className="w-[30px] h-[30px] border border-stone-800 relative left-0 bg-green-400 rounded-full "></div>
                 <div className="w-[30px] h-[30px] border border-stone-800 relative left-[-10px] bg-pink-400 rounded-full"></div>
@@ -16,23 +18,32 @@ const ContentBody = () => {
                 <div className="w-[30px] h-[30px] border border-stone-800 relative left-[-40px] bg-blue-400 rounded-full"></div>
                 <div className="w-[30px] h-[30px] border border-stone-800 relative left-[-50px] bg-rose-400 rounded-full"></div>
                 <div className="w-[30px] h-[30px] border border-stone-800 relative left-[-60px] bg-emerald-400 rounded-full"></div>
-                <div className="w-[30px] h-[30px] border border-stone-800 relative left-[-70px] bg-orange-400 rounded-full"></div>
               </div>
             </div>
           </div>
         </div>
-        <div className="flex w-[75%] justify-end items-center p-10 mr-2 gap-2">
-          <button className=" bg-blue-600 px-4 py-2 text-white font-medium rounded-lg">
+        <div className="flex w-[75%] h-[18%] justify-end items-end p-10 gap-2">
+          <button className=" bg-blue-600 px-4 py-2 text-white font-normal rounded-lg">
             Add to Story
           </button>
-          <button className=" bg-stone-600 px-4 py-2 text-white font-medium rounded-lg">
+          <button className=" bg-stone-600 px-4 py-2 text-white font-normal rounded-lg">
             Edit Profile
           </button>
-          <button className="w-[45px] h-[40px] bg-stone-600 px-4 py-2 text-white font-bold rounded-lg">
-            
-          </button>
+          <button className="w-[45px] h-[40px] bg-stone-600 px-4 py-2 text-white font-bold rounded-lg"></button>
+        </div>
+        <div className="w-[70%] border-b-1 border-slate-500"></div>
+        <div className="px-4 flex justify-start w-[70%] text-md font-medium gap-7">
+          <div className="text-slate-200 py-2">Post</div>
+          <div className="text-slate-200 py-2">About</div>
+          <div className="text-slate-200 py-2">Friends</div>
+          <div className="text-slate-200 py-2">Photos</div>
+          <div className="text-slate-200 py-2">Videos</div>
+          <div className="text-slate-200 py-2">Reels</div>
+          <div className="text-slate-200 py-2">More</div>
         </div>
       </div>
+      <div className="bg-neutral-800 h-screen"></div>
+      <LowerContent></LowerContent>
     </>
   );
 };
---
+++ src/components/LowerContent.tsx
@@ -0,0 +1,10 @@
+
+const LowerContent = () => {
+  return (
+    <>
+    <div></div>
+    </>
+  )
+}
+
+export default LowerContent
